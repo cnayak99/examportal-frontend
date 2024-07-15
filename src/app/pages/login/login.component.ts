5cf3d43ae7fa10f6494bd235ffd06f2175e51bd8
Chinmay Nayak <nayakchinmay673@gmail.com>
2024-07-14 20:36:34 -0400

    create login services

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from '../../services/login.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +18,50 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     FormsModule,
     MatCardModule,
+    CommonModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginData = {
+    username: '',
+    password: '',
+  };
+  constructor(private snack: MatSnackBar, private login: LoginService) {}
+  ngOnInit(): void {}
+  formSubmit() {
+    console.log('login button click');
+    if (
+      this.loginData.username.trim() == '' ||
+      this.loginData.username == null
+    ) {
+      this.snack.open('Username is required', '', {
+        duration: 3000,
+      });
+      return;
+    }
+    if (
+      this.loginData.password.trim() == '' ||
+      this.loginData.password == null
+    ) {
+      this.snack.open('Password is required', '', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    //request to server to generate token
+
+    this.login.generateToken(this.loginData).subscribe(
+      (data: any) => {
+        console.log('success');
+        console.log(data);
+      },
+      (error) => {
+        console.log('Error!');
+        console.log(error);
+      }
+    );
+  }
+}

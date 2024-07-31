282dfb19b9677832daa895bfe9b62a3fcca1a161
Chinmay Nayak <nayakchinmay673@gmail.com>
2024-07-31 01:48:01 -0400

    implement timer

--- src/app/pages/user/start/start.component.ts
+++ src/app/pages/user/start/start.component.ts
@@ -12,6 +12,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @Component({
   selector: 'app-start',
   standalone: true,
@@ -22,6 +25,8 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     RouterModule,
     CommonModule,
+    FormsModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.css',
@@ -29,6 +34,15 @@ import { RouterModule } from '@angular/router';
 export class StartComponent implements OnInit {
   qId: any;
   questions: any;
+  timer: any;
+
+  marksGot = 0;
+  correctAnswer = 0;
+  attempted = 0;
+  isSubmitted = false;
+  totalMarks = 0;
+  totalQuestions = 0;
+
   constructor(
     private locationSt: LocationStrategy,
     private _route: ActivatedRoute,
@@ -45,6 +59,14 @@ export class StartComponent implements OnInit {
     this._question.getQuestionsOfQuizForTest(this.qId).subscribe(
       (data: any) => {
         this.questions = data;
+        this.totalMarks = this.questions[0].quiz.maxMarks;
+        this.totalQuestions = this.questions.length;
+        this.timer = this.questions.length * 2 * 60;
+        this.questions.forEach((q: any) => {
+          q['givenAnswer'] = '';
+        });
+
+        this.startTimer();
       },
       (error) => {
         console.log(error);
@@ -58,5 +80,54 @@ export class StartComponent implements OnInit {
     this.locationSt.onPopState(() => {
       history.pushState('', '', location.href);
     });
+  }
+
+  submitQuiz() {
+    Swal.fire({
+      title: 'Do you want to submit the quiz?',
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+      icon: 'info',
+    }).then((e) => {
+      if (e.isConfirmed) {
+        this.evalQuiz();
+      }
+    });
+  }
+
+  startTimer() {
+    let t: any = window.setInterval(() => {
+      if (this.timer <= 0) {
+        this.evalQuiz();
+        clearInterval(t);
+      } else {
+        this.timer--;
+      }
+    }, 1000);
+  }
+
+  getFormattedTime() {
+    let mm = Math.floor(this.timer / 60);
+    let ss = this.timer - mm * 60;
+    return `${mm} min : ${ss} sec`;
+  }
+
+  evalQuiz() {
+    this.isSubmitted = true;
+    this.questions.forEach((q: any) => {
+      if (q.givenAnswer == q.answer) {
+        this.correctAnswer++;
+        let singleMark =
+          this.questions[0].quiz.maxMarks / this.questions.length;
+        this.marksGot += singleMark;
+      }
+      if (q.givenAnswer.trim() != '') {
+        this.attempted++;
+      }
+    });
+    console.log('Correct Answer :' + this.correctAnswer);
+    console.log('Marks got :' + this.marksGot);
+    console.log(this.questions);
+    console.log(this.attempted);
   }
 }
